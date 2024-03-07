c5daa6e9dcae481536b1a3311a583ebcb992d69e
Hasan Eray Uçar <79917113+erayucar@users.noreply.github.com>
2024-03-07 15:05:47 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,97 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.devtools.ksp")
+
+}
+
+android {
+    namespace = "com.erayucar.kefabonelik"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.erayucar.kefabonelik"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures{
+        viewBinding = true
+    }
+}
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+
+    // Fragment
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+    // Navigation Component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation( "androidx.navigation:navigation-dynamic-features-fragment:2.7.5")
+
+
+    // Room
+    implementation("androidx.room:room-runtime:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+    implementation("androidx.databinding:viewbinding:8.2.0")
+    implementation("com.google.android.gms:play-services-ads:22.6.0")
+    annotationProcessor("androidx.room:room-compiler:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+
+
+
+    implementation ("com.airbnb.android:lottie:6.0.0")
+
+    implementation ("org.jsoup:jsoup:1.14.3")
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.47")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    ksp("com.google.dagger:hilt-android-compiler:2.47")
+
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+
+    implementation ("com.squareup.picasso:picasso:2.8")
+
+
+    implementation("com.jakewharton.threetenabp:threetenabp:1.3.0")
+
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
